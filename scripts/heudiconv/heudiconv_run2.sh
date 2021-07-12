3ca31c1aa8c65c248def6e9400b17a43621568b8
vincent <vincent.w.qing@gmail.com>
2021-07-12 17:46:29 -0400

    PPMI heudiconv_r2 update.

--- scripts/heudiconv/heudiconv_run2.sh
+++ scripts/heudiconv/heudiconv_run2.sh
@@ -2,36 +2,36 @@
 DATA_NAME=(${@:1:1})
 echo ${DATA_NAME}
 COV_MODE=(${@:2:1})
-echo ${DATA_NAME}
+echo ${COV_MODE}
 
 WD_NAME="scratch"
 LOG_FILE=${DATA_NAME}_heudiconv_run2.log
-HEURISTIC_FILE="src/Heuristics_Abbas_all_T1_T2_fMRI_DTI_SWI.py"
 WD_DIR=${HOME}/${WD_NAME}
 DATA_DIR=${WD_DIR}/${DATA_NAME}
-CODE_DIR=${WD_DIR}/src/heudiconv_run2.slurm
-SUB_LIST=${WD_DIR}/${DATA_NAME}_subjects.list
+CODE_DIR=${WD_DIR}/ET_biomarker/scripts/heudiconv/heudiconv_run2.slurm
+SUB_LIST=${DATA_DIR}_subjects.list
+CON_IMG=${WD_DIR}/container_images/heudiconv_v0.8.0.simg
 
-BIDS_DIR=${DATA_DIR}_BIDS
-SLURM_LOG_OUT_DIR=${DATA_DIR}_Heudiconv_SLURM_LOG_OUT_r2
-CON_IMG_DIR=${WD_DIR}/container_images
-
-#Get subject ID
-if [ ${DATA_NAME} = 'PPMI' ]
-then
-    DATA_DIR_LEN_OFFSET=32
-    HEURISTIC_FILE="src/Heuristics_Abbas_all_T1_T2_fMRI_DTI_SWI.py"
-elif [ ${DATA_NAME} = 'ADNI' ]
-then
-    DATA_DIR_LEN_OFFSET=32
-elif [ ${DATA_NAME} = 'ADNI' ]
-then
-    DATA_DIR_LEN_OFFSET=32
-elif [ ${DATA_NAME} = 'ADNI' ]
-then
-    DATA_DIR_LEN_OFFSET=32
+#Select the proper heuristic file
+if [ ${DATA_NAME} = 'PPMI' ]; then
+    if [ ${COV_MODE} = 'T1' ]; then
+        HEURISTIC_FILE="Heuristics_PPMI_T1.py"
+    else
+        HEURISTIC_FILE="Heuristics_PPMI_all.py"
+    fi
+elif [ ${DATA_NAME} = 'ADNI' ]; then
+    if [ ${COV_MODE} = 'T1' ]; then
+        HEURISTIC_FILE="Heuristics_ADNI_T1.py"
+    else
+        HEURISTIC_FILE="Heuristics_ADNI_all.py"
+    fi
+else
+    if [ ${COV_MODE} = 'T1' ]; then
+        HEURISTIC_FILE="Heuristics_MNI-ET_T1.py"
+    else
+        HEURISTIC_FILE="Heuristics_MNI-ET_all.py"
+    fi
 fi
-
 
 RUN_ID=$(tail -c 9 ${DATA_NAME}_heudiconv_run2.log)
 if [ -z $RUN_ID ];then
@@ -42,6 +42,5 @@ else
   rm heudic_r2_vin-${RUN_ID}*.err
 fi
 
-
 # submit batch job
-sbatch ${CODE_DIR} ${DATA_NAME} ${HEURISTIC_FILE} ${CON_IMG_DIR} ${SUB_LIST}>> ${LOG_FILE}
+sbatch {CODE_DIR} ${DATA_NAME} ${HEURISTIC_FILE} ${CON_IMG} ${SUB_LIST}>> ${LOG_FILE}
