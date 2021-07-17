ce576a8fe2b4d12d9d3e765d0ed8ca4f72e85cc5
vincent <vincent.w.qing@gmail.com>
2021-07-16 22:54:41 -0400

    single subject fMRIPrep code updated.

---
+++ scripts/fmriprep/fmriprep_anat_sub.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+DATA_NAME=(${@:1:1})
+echo ${DATA_NAME}
+
+WD_DIR=${HOME}/scratch
+DATA_DIR=${WD_DIR}/${DATA_NAME}
+BIDS_DIR=${DATA_DIR}_BIDS
+CODE_DIR=${WD_DIR}/ET_biomarker/scripts/fmriprep
+
+CODE_SLURM=${CODE_DIR}/fmriprep_anat_sub.slurm
+CODE_COLLECT=${CODE_DIR}/fmriprep_anat_sub.format
+
+CON_IMG=${WD_DIR}/container_images/fmriprep_v20.2.0.simg
+#DERIVS_DIR=${DATA_DIR}_fmriprep_anat_20.2.0
+LOG_DIR=${DATA_DIR}_fmriprep_anat.log
+
+chmod +x ${CODE_SLURM}
+chmod +x ${CODE_COLLECT}
+
+echo "Step1: subjects folder created!"
+echo "Step2: starting fmriprep-SP!"
+echo "Submitting sub-1690, sub-1920, sub-2400, sub-3900 ses-2!"
+
+# err 22688556
+SUB_ID=3274
+sbatch ${CODE_SLURM} ${DATA_NAME} ${CON_IMG} ${SUB_ID} >> ${LOG_DIR}
+# err 22686840
+SUB_ID=3523
+sbatch ${CODE_SLURM} ${DATA_NAME} ${CON_IMG} ${SUB_ID} >> ${LOG_DIR}
+# err 22688314
+SUB_ID=3765
+sbatch ${CODE_SLURM} ${DATA_NAME} ${CON_IMG} ${SUB_ID} >> ${LOG_DIR}
+echo "Submitting sub-1690, sub-1920, sub-2400, sub-3900 ses-3!"
+
+echo "Submission finished!"
